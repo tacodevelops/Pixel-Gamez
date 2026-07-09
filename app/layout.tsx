f36b2dee5bcbb5efbfcd61112d70e5d98ed27fef
TacoDevelopz <qrmooney@outlook.com>
2026-07-09 20:27:34 +1200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,20 +25,25 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/images/logo/PixelGamezLogoNoBackround.png" type="image/png" />
         <link rel="apple-touch-icon" href="/images/logo/PixelGamezLogoNoBackround.png" />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-FL7J3KLSH5"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="gtm-script" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-FL7J3KLSH5');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P3R9BX7H');
           `}
         </Script>
       </head>
       <body suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P3R9BX7H"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || 'dummy_client_id_please_configure_in_env'}>
           <I18nProvider>
             <AuthProvider>
